34c1a67c69fb2ef922f2b63ffc7ae940fdce862d
Michael Charland <mike_ch_1@hotmail.com>
2013-01-14 00:05:53 -0500

    test project setup

---
+++ FiveStarsTest/src/main/java/ca/charland/fivestars/FiveStarsTest/App.java
@@ -0,0 +1,13 @@
+package ca.charland.fivestars.FiveStarsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
